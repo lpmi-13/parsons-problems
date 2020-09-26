8665a0f776313141e9fbf53e5ce28ef8cf8b2608
Adam Leskis <leskis@gmail.com>
2020-09-26 16:43:48 +0100

    update some names

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -11,6 +11,8 @@ const SplashScreen = () => {
             <p className="welcome-text">
               welcome to the examples! Choose a code type to see examples from real github projects.
               Click on the github link to go directly to the line in the original source code.
+
+              You can drag and drop the code lines to re-order them.
             </p>
             <div className="type-links">
               {codeTypes.map(type => {

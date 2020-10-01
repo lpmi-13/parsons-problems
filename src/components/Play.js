cd5606de4356df82cc41dfff17ab500baa9ad15a
Adam Leskis <leskis@gmail.com>
2020-10-01 23:49:07 +0100

    force re-shuffle if some items return to their original spot
    since the point of the activity is to give learners practice putting lines in the correct order, it makes it artificially easier if some of the lines end up back in the same place they started (meaning part of the "correct" answer is already supplied to the student).
    
    So I added a very hacky recursive loop to force a re-shuffle if any of the lines end up back in their original position after the initial shuffle.

--- src/components/Play.js
+++ src/components/Play.js
@@ -8,8 +8,10 @@ import CodeSnippet from './CodeSnippet';
 import data from '../data/function-results.json';
 import '../styles/play.scss';
 
-// some good old Fisher-Yates
+// some good old Fisher-Yates with a very hacky recursion to force no item to go back to its original position
 const shuffle = arr => {
+  let arrCopy = [...arr]
+
   var i = arr.length, j, temp;
   while(--i > 0){
     j = Math.floor(Math.random()*(i+1));
@@ -17,7 +19,22 @@ const shuffle = arr => {
     arr[j] = arr[i];
     arr[i] = temp;
   }
-  return arr;
+
+  let completelyRandomized = true;
+  // in order to be a good exercise, we don't want any of the lines to show up in the same place
+  // as the original code snippet, and sometimes after a sort, they can move back to where they started,
+  // so if any of them do that, we force a re-shuffle until they're all in a different place from
+  // the original
+  arr.forEach((element, index) => {
+    if (arr[index] === arrCopy[index]) {
+      completelyRandomized = false;
+    }
+  })
+  if (completelyRandomized) {
+    return arr;
+  } else {
+    return shuffle(arrCopy);
+  }
 }
 
 const PAUSE_LENGTH = 1000;
@@ -89,7 +106,7 @@ const Play = (props) => {
         <a className="github-link" href={`${direct_link_to_file_line}`} target="_blank" rel="noopener noreferrer">
           See it on github <FontAwesomeIcon icon={faGithub} />
         </a>
-        <div role="button" aria-label="public or private" className="swipe-arrows">
+        <div role="button" className="swipe-arrows">
           <div className="left-arrow" onClick={() => handleArrowClick(indexToMoveBackTo)}>
             <FontAwesomeIcon icon={faArrowLeft} />
           </div>
